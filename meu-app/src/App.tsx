4b5c9ce88b2297086b1ed6b45a5c1d10919de8de
goettems <gabrielggoettems@gmail.com>
2026-05-20 19:36:30 -0300

    commit q to fazendo

--- meu-app/src/App.tsx
+++ meu-app/src/App.tsx
@@ -1,30 +1,14 @@
 
-import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [conta , setcontador] = useState<number>(  90 );
 
 
    return (
   <>
     <div>
-      João Eduardo Konzen De Oliviera
-      <div>contador:{conta}</div>
+     Gabriel Gustavo Goettems
     </div>
-    <div><button onClick={()=> setcontador(conta - 1)}>
-      -contador
-    </button>
-    </div>
-    <div><button onClick={()=> setcontador(conta + 1)}>
-      +contador
-    </button>
-    </div>
-
-    <div>
-      Hello World
-    </div>
-    
   </>
   )
 }
