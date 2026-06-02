4957e691dcf9968b671cc51bf959220729c6e3fb
goettems <gabrielggoettems@gmail.com>
2026-06-02 20:56:36 -0300

    Junção de Login e Cadastro arrumada2.0

---
+++ meu-app/src/pages/public/home/Home.tsx
@@ -0,0 +1,15 @@
+ import styles from './Home.module.css';
+
+
+function Home() {
+
+
+  return (
+    <div className={styles.body}>
+      <h1>Bem-vindo à Home</h1>
+
+
+    </div>
+  );
+}
+export default Home;
