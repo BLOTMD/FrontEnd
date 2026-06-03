ddc62a252ef909eec836e94859bc41fb12392bc5
goettems <gabrielggoettems@gmail.com>
2026-06-02 22:28:51 -0300

    Home inicio

--- meu-app/src/App.tsx
+++ meu-app/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/public/login/Login';
 import Cadastro from './pages/public/register/Cadastro';
-
+import Home from './pages/public/home/Home';
 function App() {
 
 
@@ -14,6 +14,7 @@ function App() {
     <Routes>
       <Route path="/cadastro" element={<Cadastro />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
 
     </div>
--- meu-app/src/pages/public/home/Home.tsx
+++ meu-app/src/pages/public/home/Home.tsx
@@ -1,15 +1,50 @@
- import styles from './Home.module.css';
-
+import styles from "./Home.module.css";
+import logo from "../Images/LogoEasyPCesquematizada.png";
 
 function Home() {
-
-
   return (
-    <div className={styles.body}>
-      <h1>Bem-vindo à Home</h1>
+    <div className={styles.home}>
+      <header className={styles.header}>
 
+        <nav>
+          <a href="#">Início</a>
+          <a href="#">Produtos</a>
+          <a href="#">Serviços</a>
+          <a href="#">Contato</a>
+        </nav>
+      </header>
 
+      <section className={styles.Acesso}>
+        <div>
+          <img src={logo} alt="Logo da EasyPC" className={styles.logo} />
+          <h1>Monte o PC dos seus sonhos</h1>
+          <p>
+            Peças, computadores gamer com os
+            melhores preços.
+          </p>
+
+          <button className={styles.AcessoButton}>Acessar</button>
+        </div>
+      </section>
+
+      <section className={styles.cards}>
+        <div className={styles.card}>
+          <h2>Placas de Vídeo</h2>
+          <p>RTX, RX e muito mais.</p>
+        </div>
+
+        <div className={styles.card}>
+          <h2>Processadores</h2>
+          <p>Intel e AMD.</p>
+        </div>
+
+        <div className={styles.card}>
+          <h2>Computadores</h2>
+          <p>Prontos para jogar.</p>
+        </div>
+      </section>
     </div>
   );
 }
+
 export default Home;
