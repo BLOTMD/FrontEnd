93f7986e6ec823dfb1f73dd81edbd20f162bfb81
João Eduardo <blotfinalboss@gmail.com>
2026-06-10 21:42:45 -0300

    slidebar

--- meu-app/src/App.tsx
+++ meu-app/src/App.tsx
@@ -5,8 +5,7 @@ import Login from './pages/public/login/Login';
 import Cadastro from './pages/public/register/Cadastro';
 
 import Home from './pages/public/Home/Home';
-import MainLayout from "./layouts/MainLayout";
-
+import MainLayout from './layout/mainLayout';
 import carrinho from './pages/public/carrinho/carrinho';
 import comparacao from './pages/public/comparacao/comparacao';
 import loja from './pages/public/loja/loja';
@@ -20,14 +19,14 @@ function App() {
   <div>
 
     <Routes>
-      <Route path="/cadastro" element={<Cadastro />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<Home />} />
       <Route element={<MainLayout />}>
           <Route path="/loja" element={<loja />} />
           <Route path="/comparacao" element={<comparacao />} />
           <Route path="/carrinho" element={<carrinho />} />
-      </Route>
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Route>
 
     </Routes>
 
---
+++ meu-app/src/pages/sideBarAbas/Carrinho.tsx
@@ -0,0 +1,5 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./Login.module.css";
+import { useState } from "react";
+import { Service } from "../../../components/services/services";
+import type { LoginInterface } from "../../../interfaces/Login";
